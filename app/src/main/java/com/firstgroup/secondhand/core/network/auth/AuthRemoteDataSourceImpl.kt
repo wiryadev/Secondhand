793ed76ecff003d6b87c6bd239ba72f31b84888a
wiryadev <abrarwiryawan@gmail.com>
2022-06-13 22:13:18 +0700

    create AuthRemoteDataSource

---
+++ app/src/main/java/com/firstgroup/secondhand/core/network/auth/AuthRemoteDataSourceImpl.kt
@@ -0,0 +1,28 @@
+package com.firstgroup.secondhand.core.network.auth
+
+import com.firstgroup.secondhand.core.common.dispatchers.AppDispatcher
+import com.firstgroup.secondhand.core.common.dispatchers.SecondhandDispatchers
+import com.firstgroup.secondhand.core.network.auth.model.LoginResponse
+import com.firstgroup.secondhand.core.network.auth.retrofit.AuthApiService
+import com.firstgroup.secondhand.core.network.utils.RequestUtil
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class AuthRemoteDataSourceImpl @Inject constructor(
+    private val authApiService: AuthApiService,
+    @AppDispatcher(SecondhandDispatchers.IO) private val ioDispatcher: CoroutineDispatcher,
+) : AuthRemoteDataSource {
+
+    override suspend fun login(email: String, password: String): LoginResponse {
+        return withContext(ioDispatcher) {
+            authApiService.login(
+                loginData = RequestUtil.createJsonRequestBody(
+                    "email" to email,
+                    "password" to password,
+                )
+            )
+        }
+    }
+
+}
