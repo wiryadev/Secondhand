60cd3cc70bffee618864afa660a04a2db5064d2f
wiryadev <abrarwiryawan@gmail.com>
2022-06-14 12:39:32 +0700

    add register feature

--- app/src/main/java/com/firstgroup/secondhand/core/network/auth/AuthRemoteDataSourceImpl.kt
+++ app/src/main/java/com/firstgroup/secondhand/core/network/auth/AuthRemoteDataSourceImpl.kt
@@ -1,28 +1,31 @@
 package com.firstgroup.secondhand.core.network.auth
 
-import com.firstgroup.secondhand.core.common.dispatchers.AppDispatcher
-import com.firstgroup.secondhand.core.common.dispatchers.SecondhandDispatchers
+import com.firstgroup.secondhand.core.network.auth.model.AuthUserRequest
 import com.firstgroup.secondhand.core.network.auth.model.LoginResponse
+import com.firstgroup.secondhand.core.network.auth.model.UserResponse
 import com.firstgroup.secondhand.core.network.auth.retrofit.AuthService
 import com.firstgroup.secondhand.core.network.utils.RequestUtil
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class AuthRemoteDataSourceImpl @Inject constructor(
     private val authService: AuthService,
-    @AppDispatcher(SecondhandDispatchers.IO) private val ioDispatcher: CoroutineDispatcher,
 ) : AuthRemoteDataSource {
 
     override suspend fun login(email: String, password: String): LoginResponse {
-        return withContext(ioDispatcher) {
-            authService.login(
-                loginData = RequestUtil.createJsonRequestBody(
-                    "email" to email,
-                    "password" to password,
-                )
+        return authService.login(
+            loginData = RequestUtil.createJsonRequestBody(
+                "email" to email,
+                "password" to password,
             )
-        }
+        )
+    }
+
+    override suspend fun register(authUserRequest: AuthUserRequest): UserResponse {
+        val formData = authUserRequest.toFormData()
+        return authService.register(
+            formData.requestBody,
+            formData.multipart,
+        )
     }
 
 }
