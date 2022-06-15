e2f62f43839869424ef4d9867fc44aab8be3403d
hus n i <husnia322@gmail.com>
2022-06-15 23:48:30 +0700

    create login screen (login succes -> move fragment implemented)

--- app/src/main/java/com/firstgroup/secondhand/ui/auth/login/LoginFragment.kt
+++ app/src/main/java/com/firstgroup/secondhand/ui/auth/login/LoginFragment.kt
@@ -1,7 +1,198 @@
 package com.firstgroup.secondhand.ui.auth.login
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.firstgroup.secondhand.R
+import com.firstgroup.secondhand.ui.components.TopSnackBar
+import com.google.android.material.composethemeadapter.MdcTheme
+import dagger.hilt.android.AndroidEntryPoint
 
-class LoginFragment: Fragment() {
+@AndroidEntryPoint
+class LoginFragment : Fragment() {
 
+    private val viewModel: LoginViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MdcTheme {
+                    LoginScreen(
+                        viewModel = viewModel,
+                        toHome = { findNavController().navigate(R.id.action_loginFragment_to_registerFragment) })
+                }
+            }
+        }
+    }
 }
+
+@Composable
+fun LoginScreen(viewModel: LoginViewModel, toHome: () -> Unit) {
+    val uiState by viewModel.uiState.collectAsState()
+
+    LoginScreen(
+        uiState = uiState,
+        onSnackbarDismissed = {},
+        onLoginClick = { email, password ->
+            viewModel.login(email = email, password = password)
+        },
+        toHome = toHome
+    )
+}
+
+@Composable
+fun LoginScreen(
+    uiState: LoginUiState,
+    onLoginClick: (String, String) -> Unit,
+    onSnackbarDismissed: () -> Unit,
+    toHome: () -> Unit
+) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp)
+        ) {
+
+            Text(
+                text = "Masuk",
+                style = MaterialTheme.typography.h5,
+                fontWeight = FontWeight.ExtraBold,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 53.dp, bottom = 12.dp)
+            )
+
+            Text(
+                text = "Email",
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp, bottom = 2.dp)
+            )
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                textStyle = MaterialTheme.typography.body1,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp, top = 2.dp)
+                    .border(
+                        1.dp,
+                        colorResource(id = R.color.neutral_02),
+                        RoundedCornerShape(16.dp)
+                    ),
+                shape = RoundedCornerShape(16.dp),
+                singleLine = true,
+                placeholder = {
+                    Text(
+                        text = "Your Email",
+                        style = MaterialTheme.typography.body1,
+                    )
+                },
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.White,
+                    placeholderColor = colorResource(id = R.color.neutral_02)
+                ),
+            )
+
+            Text(
+                text = "Password",
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp, bottom = 2.dp)
+            )
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                textStyle = MaterialTheme.typography.body1,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 12.dp, top = 2.dp)
+                    .border(
+                        1.dp,
+                        colorResource(id = R.color.neutral_02),
+                        RoundedCornerShape(16.dp)
+                    ),
+                shape = RoundedCornerShape(16.dp),
+                singleLine = true,
+                placeholder = {
+                    Text(
+                        text = "Your Password",
+                        style = MaterialTheme.typography.body1,
+                    )
+                },
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.White,
+                    placeholderColor = colorResource(id = R.color.neutral_02)
+                ),
+            )
+
+            Button(
+                onClick = {
+                    onLoginClick(email, password)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp)
+                    .size(height = 48.dp, width = 0.dp),
+                shape = RoundedCornerShape(16.dp),
+                enabled = !uiState.isLoading
+            ) {
+                Text(
+                    text = if (uiState.isLoading) "Loading" else "Register",
+                )
+            }
+        }
+        uiState.errorMessage?.let {
+            TopSnackBar(
+                message = stringResource(id = it),
+                isError = true,
+                onDismissClick = onSnackbarDismissed,
+                modifier = Modifier.align(Alignment.TopCenter)
+            )
+        }
+        if (uiState.isSuccess) {
+            TopSnackBar(
+                message = stringResource(id = R.string.login_success),
+                isError = false,
+                onDismissClick = {
+                    onSnackbarDismissed()
+                    toHome()
+                }
+            )
+        }
+    }
+}
+
+//@Preview(showBackground = true, showSystemUi = true)
+//@Composable
+//fun LoginPreview() {
+////    MdcTheme { LoginScreen() }
+//}
