103007d287a9bec95cbb93a7a5518caac5a63363
wiryadev <abrarwiryawan@gmail.com>
2022-06-16 10:03:08 +0700

    add table to database and add dao to module

--- app/src/main/java/com/firstgroup/secondhand/core/database/SecondhandDatabase.kt
+++ app/src/main/java/com/firstgroup/secondhand/core/database/SecondhandDatabase.kt
@@ -6,10 +6,12 @@ import com.firstgroup.secondhand.core.database.product.dao.CategoryDao
 import com.firstgroup.secondhand.core.database.product.dao.ProductCacheDao
 import com.firstgroup.secondhand.core.database.product.dao.WishlistDao
 import com.firstgroup.secondhand.core.database.product.entity.CategoryEntity
+import com.firstgroup.secondhand.core.database.product.entity.ProductEntity
 import com.firstgroup.secondhand.core.database.product.entity.WishlistEntity
 
 @Database(
     entities = [
+        ProductEntity::class,
         CategoryEntity::class,
         WishlistEntity::class,
     ],
