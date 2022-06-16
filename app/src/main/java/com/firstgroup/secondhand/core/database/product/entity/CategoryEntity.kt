c82ffbef9c868340294bacca01bd62962e0ee82d
wiryadev <abrarwiryawan@gmail.com>
2022-06-16 09:33:08 +0700

    add mapper to `CategoryEntity`

--- app/src/main/java/com/firstgroup/secondhand/core/database/product/entity/CategoryEntity.kt
+++ app/src/main/java/com/firstgroup/secondhand/core/database/product/entity/CategoryEntity.kt
@@ -3,6 +3,7 @@ package com.firstgroup.secondhand.core.database.product.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.firstgroup.secondhand.core.model.Category
 
 @Entity(tableName = "product_categories")
 data class CategoryEntity(
@@ -12,4 +13,9 @@ data class CategoryEntity(
 
     @ColumnInfo(name = "name")
     val name: String,
-)
+) {
+    fun mapToDomainModel() = Category(
+        id = id,
+        name = name,
+    )
+}
