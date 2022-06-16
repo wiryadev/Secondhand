3fb5f65a6f076d15285276034b86b85d12c4b4c1
wiryadev <abrarwiryawan@gmail.com>
2022-06-16 14:22:04 +0700

    create auth repository unit test for login

---
+++ app/src/test/java/com/firstgroup/secondhand/core/data/repositories/auth/AuthRepositoryTest.kt
@@ -0,0 +1,56 @@
+package com.firstgroup.secondhand.core.data.repositories.auth
+
+import com.firstgroup.secondhand.core.model.Authentication
+import com.firstgroup.secondhand.core.network.auth.AuthRemoteDataSource
+import com.firstgroup.secondhand.core.network.auth.model.LoginResponse
+import com.firstgroup.secondhand.core.preference.AuthPreferenceDataSource
+import com.firstgroup.secondhand.utils.TestDispatcherRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+@ExperimentalCoroutinesApi
+class AuthRepositoryTest {
+
+    @get:Rule
+    val dispatcherRule = TestDispatcherRule()
+
+    private lateinit var remoteDataSource: AuthRemoteDataSource
+    private lateinit var preferenceDataSource: AuthPreferenceDataSource
+    private lateinit var repository: AuthRepository
+
+    @Before
+    fun setUp() {
+        remoteDataSource = mock()
+        preferenceDataSource = mock()
+        repository = AuthRepositoryImpl(remoteDataSource, preferenceDataSource)
+    }
+
+    @Test
+    fun login_whenAllInputValid_thenReturnAuthenticationModel() = runTest {
+        val response = LoginResponse(
+            accessToken = "token",
+            email = "email",
+            name = "name",
+        )
+        val expected = Authentication(
+            token = "token",
+            email = "email",
+            name = "name",
+        )
+
+        whenever(remoteDataSource.login("email", "password"))
+            .thenReturn(response)
+
+        val actual = repository.login("email", "password")
+
+        verify(remoteDataSource).login("email", "password")
+        assertEquals(expected, actual)
+    }
+}
