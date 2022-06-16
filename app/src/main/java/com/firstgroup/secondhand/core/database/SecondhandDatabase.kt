0040c539d6a1b26c5851d2b2bf9ac2a5b750b517
wiryadev <abrarwiryawan@gmail.com>
2022-06-16 09:34:12 +0700

    setup data layer for product feature

--- app/src/main/java/com/firstgroup/secondhand/core/database/SecondhandDatabase.kt
+++ app/src/main/java/com/firstgroup/secondhand/core/database/SecondhandDatabase.kt
@@ -3,6 +3,7 @@ package com.firstgroup.secondhand.core.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.firstgroup.secondhand.core.database.product.dao.CategoryDao
+import com.firstgroup.secondhand.core.database.product.dao.ProductCacheDao
 import com.firstgroup.secondhand.core.database.product.dao.WishlistDao
 import com.firstgroup.secondhand.core.database.product.entity.CategoryEntity
 import com.firstgroup.secondhand.core.database.product.entity.WishlistEntity
@@ -15,6 +16,7 @@ import com.firstgroup.secondhand.core.database.product.entity.WishlistEntity
     version = 1,
 )
 abstract class SecondhandDatabase : RoomDatabase() {
+    abstract fun productCacheDao(): ProductCacheDao
     abstract fun categoryDao(): CategoryDao
     abstract fun wishlistDao(): WishlistDao
 }
---
+++ app/src/main/java/com/firstgroup/secondhand/core/database/product/entity/ProductEntity.kt
@@ -0,0 +1,49 @@
+package com.firstgroup.secondhand.core.database.product.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.firstgroup.secondhand.core.model.Product
+
+@Entity(tableName = "product_cache")
+data class ProductEntity(
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    val id: Int,
+
+    @ColumnInfo(name = "name")
+    val name: String,
+
+    @ColumnInfo(name = "description")
+    val description: String,
+
+    @ColumnInfo(name = "price")
+    val price: Int,
+
+    @ColumnInfo(name = "image_url")
+    val imageUrl: String,
+
+    @ColumnInfo(name = "location")
+    val location: String,
+
+    @ColumnInfo(name = "user_id")
+    val userId: Int,
+
+    @ColumnInfo(name = "status")
+    val status: String,
+
+    @ColumnInfo(name = "categories")
+    val categories: List<CategoryEntity>,
+) {
+    fun mapToDomainModel() = Product(
+        id = id,
+        name = name,
+        description = description,
+        price = price,
+        imageUrl = imageUrl,
+        location = location,
+        userId = userId,
+        status = status,
+        categories = categories.map { it.mapToDomainModel() }
+    )
+}
