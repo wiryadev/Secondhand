b3205aee5d4cd783ee96318e153de260e739ab2c
wiryadev <abrarwiryawan@gmail.com>
2022-06-17 21:53:11 +0700

    add `ProductRemoteDataSource` to DI module

--- app/src/main/java/com/firstgroup/secondhand/core/network/di/RemoteDataSourceModule.kt
+++ app/src/main/java/com/firstgroup/secondhand/core/network/di/RemoteDataSourceModule.kt
@@ -2,6 +2,8 @@ package com.firstgroup.secondhand.core.network.di
 
 import com.firstgroup.secondhand.core.network.auth.AuthRemoteDataSource
 import com.firstgroup.secondhand.core.network.auth.AuthRemoteDataSourceImpl
+import com.firstgroup.secondhand.core.network.product.ProductRemoteDataSource
+import com.firstgroup.secondhand.core.network.product.ProductRemoteDataSourceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -17,5 +19,11 @@ interface RemoteDataSourceModule {
     fun bindsAuthRemoteDataSource(
         authRemoteDataSource: AuthRemoteDataSourceImpl
     ): AuthRemoteDataSource
+
+    @Binds
+    @Singleton
+    fun bindsProductRemoteDataSource(
+        productRemoteDataSourceImpl: ProductRemoteDataSourceImpl
+    ): ProductRemoteDataSource
 
 }
