82b710b2a0a6c3819c341dbb6a997e837383726d
wiryadev <abrarwiryawan@gmail.com>
2022-06-18 16:52:17 +0700

    update `LoginUseCase` to include injecting token interceptor

--- app/src/main/java/com/firstgroup/secondhand/domain/auth/LoginUseCase.kt
+++ app/src/main/java/com/firstgroup/secondhand/domain/auth/LoginUseCase.kt
@@ -5,18 +5,21 @@ import com.firstgroup.secondhand.core.common.dispatchers.SecondhandDispatchers.I
 import com.firstgroup.secondhand.core.data.repositories.auth.AuthRepository
 import com.firstgroup.secondhand.core.model.Authentication
 import com.firstgroup.secondhand.core.network.auth.model.LoginRequest
+import com.firstgroup.secondhand.core.network.utils.AuthInterceptor
 import com.firstgroup.secondhand.domain.UseCase
 import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Inject
 
 class LoginUseCase @Inject constructor(
     private val repository: AuthRepository,
+    private val authInterceptor: AuthInterceptor,
     @AppDispatcher(IO) private val ioDispatcher: CoroutineDispatcher,
 ) : UseCase<LoginRequest, Authentication>(ioDispatcher) {
 
     override suspend fun execute(param: LoginRequest): Authentication {
         val login = repository.login(param)
         repository.saveUserSession(login)
+        authInterceptor.setToken(login.token)
         return login
     }
 
