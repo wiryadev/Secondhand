99b695da3fd5b4d9878275aaa6e5fb235fa60e4e
wiryadev <abrarwiryawan@gmail.com>
2022-06-20 23:28:02 +0700

    add login checking in `SplashActivity`

--- app/src/main/java/com/firstgroup/secondhand/SplashActivity.kt
+++ app/src/main/java/com/firstgroup/secondhand/SplashActivity.kt
@@ -1,21 +1,66 @@
 package com.firstgroup.secondhand
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.firstgroup.secondhand.ui.auth.AuthActivity
+import com.firstgroup.secondhand.ui.main.MainActivity
+import com.google.android.material.composethemeadapter.MdcTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
 
+    private val viewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
-        startActivity(
-            Intent(this, AuthActivity::class.java)
-        )
-        finish()
+
+        checkLoginStatus()
+        setContent {
+            MdcTheme {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .fillMaxSize(),
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
+        }
+    }
+
+    private fun checkLoginStatus() {
+        viewModel.checkSession()
+        lifecycleScope.launch {
+            viewModel.uiState
+                .flowWithLifecycle(lifecycle)
+                .collect { uiState ->
+                    when (uiState) {
+                        is MainUiState.Initial -> {
+                            // do nothing until data is loaded
+                        }
+                        is MainUiState.Loaded -> {
+                            if (uiState.isLoggedIn) {
+                                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                            } else {
+                                startActivity(Intent(this@SplashActivity, AuthActivity::class.java))
+                            }
+                        }
+                    }
+                }
+        }
     }
 }
