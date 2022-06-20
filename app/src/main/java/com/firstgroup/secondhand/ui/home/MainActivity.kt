029a121a2d084930a3be3b3bde51c7dd8afa9d2c
Aldiriansyah Dwi <kurosenpai12@gmail.com>
2022-06-20 20:01:20 +0700

    rename home activity to main activity

---
+++ app/src/main/java/com/firstgroup/secondhand/ui/home/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.firstgroup.secondhand.ui.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.firstgroup.secondhand.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
