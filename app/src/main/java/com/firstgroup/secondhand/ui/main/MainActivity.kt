57cc5ab068f470480ceef6989f09af8525e88c8a
wiryadev <abrarwiryawan@gmail.com>
2022-06-21 12:06:58 +0700

    add `@AndroidEntryPoint` annotation to fix crash

--- app/src/main/java/com/firstgroup/secondhand/ui/main/MainActivity.kt
+++ app/src/main/java/com/firstgroup/secondhand/ui/main/MainActivity.kt
@@ -3,8 +3,9 @@ package com.firstgroup.secondhand.ui.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.firstgroup.secondhand.R
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
