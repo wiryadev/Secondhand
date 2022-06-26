00ad0b6b6cd7568e275528d445215616234ad3eb
wiryadev <abrarwiryawan@gmail.com>
2022-06-26 21:40:31 +0700

    remove header token from product buyer and categories

--- app/src/main/java/com/firstgroup/secondhand/core/network/product/retrofit/ProductService.kt
+++ app/src/main/java/com/firstgroup/secondhand/core/network/product/retrofit/ProductService.kt
@@ -4,6 +4,7 @@ import com.firstgroup.secondhand.core.network.product.model.AddProductDto
 import com.firstgroup.secondhand.core.network.product.model.BannerDto
 import com.firstgroup.secondhand.core.network.product.model.CategoryDto
 import com.firstgroup.secondhand.core.network.product.model.ProductDto
+import com.firstgroup.secondhand.core.network.utils.AuthInterceptor.Companion.NO_AUTH_HEADER_KEY
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.http.*
@@ -14,6 +15,7 @@ interface ProductService {
      * Buyer
      */
     @GET("buyer/product")
+    @Headers("${NO_AUTH_HEADER_KEY}: true")
     suspend fun getProductsAsBuyer(): List<ProductDto>
 
     @GET("buyer/product/{id}")
@@ -41,6 +43,7 @@ interface ProductService {
     ): AddProductDto
 
     @GET("seller/category")
+    @Headers("${NO_AUTH_HEADER_KEY}: true")
     suspend fun getCategories(): List<CategoryDto>
 
     @GET("seller/banner")
