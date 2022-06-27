53b8bbb623a1c67327410392d22706a583bd122a
Aldiriansyah Dwi <kurosenpai12@gmail.com>
2022-06-27 16:21:54 +0700

    add componen for outlined button

--- app/src/main/java/com/firstgroup/secondhand/ui/components/Button.kt
+++ app/src/main/java/com/firstgroup/secondhand/ui/components/Button.kt
@@ -1,13 +1,17 @@
 package com.firstgroup.secondhand.ui.components
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.OutlinedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
+import com.firstgroup.secondhand.R
 
 @Composable
 fun PrimaryButton(
@@ -21,6 +25,31 @@ fun PrimaryButton(
         modifier = modifier
             .fillMaxWidth()
             .heightIn(min = 48.dp),
+        shape = RoundedCornerShape(16.dp),
+        enabled = enabled,
+        contentPadding = PaddingValues(horizontal = 24.dp, vertical = 14.dp)
+    ) {
+        content()
+    }
+}
+
+@Composable
+fun SecondaryButton(
+    onClick: () -> Unit,
+    content: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+){
+    OutlinedButton(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 48.dp)
+            .border(
+                1.dp,
+                colorResource(id = R.color.dark_blue_04),
+                RoundedCornerShape(16.dp)
+            ),
         shape = RoundedCornerShape(16.dp),
         enabled = enabled,
         contentPadding = PaddingValues(horizontal = 24.dp, vertical = 14.dp)
