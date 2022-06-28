d781b845f1dcd000846e60c1b3df93ed6d89d208
hus n i <husnia322@gmail.com>
2022-06-28 12:20:52 +0700

    - Make LoginLayoutPlaceholder.kt - Assign LoginLayoutPlaceholder.kt to SellFragment.kt, NotificationFragment.kt, SellListFragment.kt.

---
+++ app/src/main/java/com/firstgroup/secondhand/ui/components/LoginLayoutPlaceholder.kt
@@ -0,0 +1,48 @@
+package com.firstgroup.secondhand.ui.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.firstgroup.secondhand.R
+
+@Composable
+fun LoginLayoutPlaceholder(
+    onButtonClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val rawLottie by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.login_and_sign_up2))
+        
+        Spacer(modifier = Modifier.weight(1f))
+        LottieAnimation(
+            composition = rawLottie,
+            modifier = Modifier.size(180.dp)
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(text = stringResource(R.string.login_placeholder_commentary_text))
+
+        Spacer(modifier = Modifier.height(48.dp))
+        PrimaryButton(
+            onClick = {
+                onButtonClick()
+            },
+            content = {
+                Text(text = stringResource(id = R.string.login))
+            }
+        )
+        Spacer(modifier = Modifier.weight(1f))
+    }
+}
--- app/src/main/java/com/firstgroup/secondhand/ui/main/notification/NotificationFragment.kt
+++ app/src/main/java/com/firstgroup/secondhand/ui/main/notification/NotificationFragment.kt
@@ -1,6 +1,44 @@
 package com.firstgroup.secondhand.ui.main.notification
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import com.firstgroup.secondhand.ui.components.LoginLayoutPlaceholder
+import com.google.android.material.composethemeadapter.MdcTheme
 
-class NotificationFragment: Fragment()  {
+class NotificationFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MdcTheme {
+                    NotificationScreen {
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun NotificationScreen(
+    onClickAction: () -> Unit,
+) {
+    // cek kondisi sudah login belum
+
+    // jika belum login LoginLayoutPlaceholder di tampilin
+    LoginLayoutPlaceholder { onClickAction() }
+
+    // jika udah login nampilin
+    // NotificationScreen() {
+    // }
 }
