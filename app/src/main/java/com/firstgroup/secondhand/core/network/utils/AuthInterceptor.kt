e7708577ece16748f764f66f35dc9b1af69e7ceb
wiryadev <abrarwiryawan@gmail.com>
2022-06-28 16:57:53 +0700

    add validation before assign new token

--- app/src/main/java/com/firstgroup/secondhand/core/network/utils/AuthInterceptor.kt
+++ app/src/main/java/com/firstgroup/secondhand/core/network/utils/AuthInterceptor.kt
@@ -1,6 +1,5 @@
 package com.firstgroup.secondhand.core.network.utils
 
-import android.util.Log
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
@@ -24,7 +23,9 @@ class AuthInterceptor : Interceptor {
     }
 
     fun setToken(newToken: String) {
-        token = newToken
+        if (newToken.isNotEmpty()) {
+            token = newToken
+        }
     }
 
     fun deleteToken() {
