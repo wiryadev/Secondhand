9f20760b22eb5d28b61018adf8afd8531ba8eef3
wiryadev <abrarwiryawan@gmail.com>
2022-06-29 21:05:28 +0700

    create Order Repository

--- app/src/main/java/com/firstgroup/secondhand/core/data/di/RepositoryModule.kt
+++ app/src/main/java/com/firstgroup/secondhand/core/data/di/RepositoryModule.kt
@@ -2,6 +2,8 @@ package com.firstgroup.secondhand.core.data.di
 
 import com.firstgroup.secondhand.core.data.repositories.auth.AuthRepository
 import com.firstgroup.secondhand.core.data.repositories.auth.AuthRepositoryImpl
+import com.firstgroup.secondhand.core.data.repositories.order.OrderRepository
+import com.firstgroup.secondhand.core.data.repositories.order.OrderRepositoryImpl
 import com.firstgroup.secondhand.core.data.repositories.product.ProductRepository
 import com.firstgroup.secondhand.core.data.repositories.product.ProductRepositoryImpl
 import dagger.Binds
@@ -25,5 +27,11 @@ interface RepositoryModule {
     fun bindsProductRepository(
         productRepositoryImpl: ProductRepositoryImpl
     ): ProductRepository
+
+    @Binds
+    @Singleton
+    fun bindsOrderRepository(
+        orderRepositoryImpl: OrderRepositoryImpl
+    ): OrderRepository
 
 }
---
+++ app/src/main/java/com/firstgroup/secondhand/core/data/repositories/order/OrderRepository.kt
@@ -0,0 +1,21 @@
+package com.firstgroup.secondhand.core.data.repositories.order
+
+import com.firstgroup.secondhand.core.model.CreateOrder
+import com.firstgroup.secondhand.core.model.Order
+import com.firstgroup.secondhand.core.network.order.model.OrderRequest
+
+interface OrderRepository {
+
+    /**
+     * Buyer
+     */
+
+    suspend fun createOrder(orderRequest: OrderRequest): CreateOrder
+
+//    suspend fun updateOrder(id: Int): CreateOrderDto
+
+    suspend fun getOrdersAsBuyer(): List<Order>
+
+    suspend fun getOrderByIdAsBuyer(id: Int): Order
+
+}
