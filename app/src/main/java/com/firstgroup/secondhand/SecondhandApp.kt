8e96adeff65e62c5ae867f873f9326877db0ae78
wiryadev <abrarwiryawan@gmail.com>
2022-07-04 13:45:51 +0700

    create custom `ImageLoader` to enable offline caching

--- app/src/main/java/com/firstgroup/secondhand/SecondhandApp.kt
+++ app/src/main/java/com/firstgroup/secondhand/SecondhandApp.kt
@@ -1,7 +1,28 @@
 package com.firstgroup.secondhand
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import com.firstgroup.secondhand.core.network.utils.CoilResponseHeaderInterceptor
 import dagger.hilt.android.HiltAndroidApp
+import okhttp3.OkHttpClient
 
 @HiltAndroidApp
-class SecondhandApp : Application()
+class SecondhandApp : Application(), ImageLoaderFactory {
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .okHttpClient {
+                OkHttpClient.Builder()
+                    .addNetworkInterceptor(
+                        CoilResponseHeaderInterceptor(
+                            name = "Cache-Control",
+                            value = "max-age=31536000,public",
+                        )
+                    )
+                    .build()
+            }
+            .build()
+    }
+
+}
