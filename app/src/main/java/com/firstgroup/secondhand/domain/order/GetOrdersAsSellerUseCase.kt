3786a21f4e8bef0caf814c8710b150e0a3d692cc
wiryadev <abrarwiryawan@gmail.com>
2022-07-08 18:28:10 +0700

    create `GetOrdersAsSellerUseCase`

---
+++ app/src/main/java/com/firstgroup/secondhand/domain/order/GetOrdersAsSellerUseCase.kt
@@ -0,0 +1,32 @@
+package com.firstgroup.secondhand.domain.order
+
+import com.firstgroup.secondhand.core.common.dispatchers.AppDispatcher
+import com.firstgroup.secondhand.core.common.dispatchers.SecondhandDispatchers.IO
+import com.firstgroup.secondhand.core.data.repositories.order.OrderRepository
+import com.firstgroup.secondhand.core.model.Order
+import com.firstgroup.secondhand.domain.UseCase
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Inject
+
+class GetOrdersAsSellerUseCase @Inject constructor(
+    private val orderRepository: OrderRepository,
+    @AppDispatcher(IO) private val ioDispatcher: CoroutineDispatcher,
+) : UseCase<OrderFilter, List<Order>>(ioDispatcher) {
+
+    override suspend fun execute(param: OrderFilter): List<Order> {
+        return when(param) {
+            OrderFilter.ALlOrders -> orderRepository.getAllOrdersAsSeller()
+            OrderFilter.AcceptedOrders -> orderRepository.getAcceptedOrdersAsSeller()
+            OrderFilter.DeclinedOrders -> orderRepository.getDeclinedOrdersAsSeller()
+            OrderFilter.PendingOrders -> orderRepository.getPendingOrdersAsSeller()
+        }
+    }
+
+}
+
+sealed interface OrderFilter {
+    object ALlOrders: OrderFilter
+    object AcceptedOrders: OrderFilter
+    object DeclinedOrders: OrderFilter
+    object PendingOrders: OrderFilter
+}
