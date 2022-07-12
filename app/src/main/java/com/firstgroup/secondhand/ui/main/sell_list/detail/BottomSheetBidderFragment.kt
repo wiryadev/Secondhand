374277987085bde0c3bd2d2bcff351b9e95d8bd9
Aldiriansyah Dwi <kurosenpai12@gmail.com>
2022-07-12 20:20:08 +0700

    add viewModel to BottomSheetBidderFragment.kt

--- app/src/main/java/com/firstgroup/secondhand/ui/main/sell_list/detail/BottomSheetBidderFragment.kt
+++ app/src/main/java/com/firstgroup/secondhand/ui/main/sell_list/detail/BottomSheetBidderFragment.kt
@@ -8,24 +8,37 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
+import androidx.fragment.app.viewModels
 import coil.compose.rememberAsyncImagePainter
 import com.firstgroup.secondhand.R
 import com.firstgroup.secondhand.ui.components.PrimaryButton
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.composethemeadapter.MdcTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class BottomSheetBidderFragment: BottomSheetDialogFragment() {
+
+    private val viewModel: BottomSheetBidderViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val id = arguments?.getInt(ORDER_ID_KEY)
+        id?.let {
+            viewModel.getOrderAsSeller(it)
+        }
     }
 
     override fun onCreateView(
@@ -35,159 +48,186 @@ class BottomSheetBidderFragment: BottomSheetDialogFragment() {
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
+                val uiState by viewModel.uiState.collectAsState()
                 MdcTheme{
-                    // bottom sheet
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier
-                            .padding(
-                                horizontal = 32.dp
-                            )
+                    BottomSheetBidderScreen(
+                        uiState = uiState
+                    )
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val ORDER_ID_KEY = "com.firstgroup.secondhand.ui.main.sell_list.detail.BottomSheetBidderFragment"
+
+        fun newInstance(orderId: Int) = BottomSheetBidderFragment().apply {
+            arguments = Bundle().apply {
+                putInt(ORDER_ID_KEY, orderId)
+            }
+        }
+    }
+}
+
+@Composable
+fun BottomSheetBidderScreen(
+    uiState: BottomSheetBidderUiState,
+){
+
+    uiState.apply {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(
+                    horizontal = 32.dp
+                )
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+            Card(
+                modifier = Modifier
+                    .size(
+                        width = 60.dp,
+                        height = 6.dp
+                    ),
+                backgroundColor = Color.Gray,
+                shape = RoundedCornerShape(20.dp)
+            ) { }
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = stringResource(R.string.bottomsheet_title),
+                style = MaterialTheme.typography.body1.copy(
+                    fontWeight = FontWeight.Bold
+                )
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = stringResource(R.string.bottomsheet_bidder_description),
+                style = MaterialTheme.typography.body1.copy(
+                    color = Color.Gray
+                )
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            // buyer and product information
+            Card(
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(16.dp),
+                elevation = 4.dp,
+            ) {
+                Column(
+                    modifier = Modifier.padding(all = 16.dp)
+                ) {
+                    Text(
+                        text = stringResource(R.string.product_match),
+                        style = MaterialTheme.typography.body1.copy(
+                            fontWeight = FontWeight.Bold
+                        )
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
                     ) {
-                        Spacer(modifier = Modifier.height(16.dp))
-                        Card(
+                        val painterBuyerImage = rememberAsyncImagePainter(
+                            model = R.drawable.img_profile_placeholder // it should user.imageUrl, but order response doesn't give any image from buyer
+                        )
+                        // seller profile image
+                        Image(
+                            painter = painterBuyerImage,
+                            contentDescription = null,
                             modifier = Modifier
-                                .size(
-                                    width = 60.dp,
-                                    height = 6.dp
-                                ),
-                            backgroundColor = Color.Gray,
-                            shape = RoundedCornerShape(20.dp)
-                        ){ }
-                        Spacer(modifier = Modifier.height(16.dp))
-                        Text(
-                            text = context.getString(R.string.bottomsheet_title),
-                            style = MaterialTheme.typography.body1.copy(
-                                fontWeight = FontWeight.Bold
-                            )
+                                .size(48.dp)
                         )
-                        Spacer(modifier = Modifier.height(8.dp))
-                        Text(
-                            text = context.getString(R.string.bottomsheet_bidder_description),
-                            style = MaterialTheme.typography.body1.copy(
-                                color = Color.Gray
-                            )
-                        )
-                        Spacer(modifier = Modifier.height(16.dp))
-                        // buyer and product information
-                        Card(
-                            modifier = Modifier.fillMaxWidth(),
-                            shape = RoundedCornerShape(16.dp),
-                            elevation = 4.dp,
+                        Column(
+                            modifier = Modifier
+                                .padding(start = 16.dp)
                         ) {
-                            Column(
-                                modifier = Modifier.padding(all = 16.dp)
-                            ) {
-                                Text(
-                                    text = context.getString(R.string.product_match),
-                                    style = MaterialTheme.typography.body1.copy(
-                                        fontWeight = FontWeight.Bold
-                                    )
-                                )
-                                Spacer(modifier = Modifier.height(16.dp))
-                                Row(
-                                    modifier = Modifier.fillMaxWidth(),
-                                    verticalAlignment = Alignment.CenterVertically
-                                ) {
-                                    val painterBuyerImage = rememberAsyncImagePainter(
-                                        model = dummyOrder.product.imageUrl // it should user.imageUrl, but order response doesn't give any image from buyer
-                                    )
-                                    // seller profile image
-                                    Image(
-                                        painter = painterBuyerImage,
-                                        contentDescription = null,
-                                        modifier = Modifier
-                                            .size(48.dp)
-                                    )
-                                    Column(
-                                        modifier = Modifier
-                                            .padding(start = 16.dp)
-                                    ) {
-                                        // text buyer full name
-                                        Text(
-                                            text = dummyOrder.user.fullName,
-                                            style = MaterialTheme.typography.body1.copy(
-                                                fontWeight = FontWeight.Bold
-                                            ),
-                                        )
-                                        // text buyer city
-                                        Text(
-                                            text = dummyOrder.user.city.toString(), // response is any
-                                            style = MaterialTheme.typography.body2.copy(
-                                                color = Color.Gray
-                                            ),
-                                            modifier = Modifier
-                                                .padding(top = 4.dp)
-                                        )
-                                    }
-                                }
-                                Spacer(modifier = Modifier.height(16.dp))
-                                Row(modifier = Modifier.fillMaxWidth()) {
-                                    // image product
-                                    val painterProductImage = rememberAsyncImagePainter(
-                                        model = dummyOrder.product.imageUrl
-                                    )
-                                    Image(
-                                        painter = painterProductImage,
-                                        contentDescription = null,
-                                        modifier = Modifier
-                                            .size(48.dp)
-                                    )
-                                    Spacer(modifier = Modifier.width(16.dp))
-                                    // product detail : name, price, bid
-                                    Column(modifier = Modifier.fillMaxWidth()) {
-                                        Spacer(modifier = Modifier.height(4.dp))
-                                        Text(
-                                            text = dummyOrder.product.name,
-                                            style = MaterialTheme.typography.body1.copy(
-                                                fontWeight = FontWeight.Bold
-                                            )
-                                        )
-                                        // product normal price
-                                        Spacer(modifier = Modifier.height(4.dp))
-                                        Text(
-                                            text = "Rp ${dummyOrder.product.basePrice}",
-                                            style = MaterialTheme.typography.body1.copy(
-                                                fontWeight = FontWeight.Bold,
-                                                textDecoration = TextDecoration.LineThrough
-                                            )
-                                        )
-                                        // buyer bid for selected product
-                                        Spacer(modifier = Modifier.height(4.dp))
-                                        Text(
-                                            text = "Ditawar Rp ${dummyOrder.price}",
-                                            style = MaterialTheme.typography.body1.copy(
-                                                fontWeight = FontWeight.Bold
-                                            )
-                                        )
-                                    }
-                                }
-                            }
+                            // text buyer full name
+                            val buyerName = order?.product?.seller?.name ?: "No Name"
+                            Text(
+                                text = buyerName,
+                                style = MaterialTheme.typography.body1.copy(
+                                    fontWeight = FontWeight.Bold
+                                ),
+                            )
+                            // text buyer city
+                            val buyerCity = order?.product?.seller?.city ?: ""
+                            Text(
+                                text = buyerCity, // response is any
+                                style = MaterialTheme.typography.body2.copy(
+                                    color = Color.Gray
+                                ),
+                                modifier = Modifier
+                                    .padding(top = 4.dp)
+                            )
                         }
-                        Spacer(modifier = Modifier.height(24.dp))
-                        // button to contact buyer via whatsapp
-                        PrimaryButton(
-                            onClick = { /*TODO whatsapp*/ },
-                            content = {
-                                Row {
-                                    Text(
-                                        text = context.getString(R.string.contact_whatsapp),
-                                        style = MaterialTheme.typography.button.copy(
-                                            textAlign = TextAlign.Center
-                                        ),
-                                        modifier = Modifier.fillMaxWidth(0.8f)
-                                    )
-                                    Icon(
-                                        painter = rememberAsyncImagePainter(model = R.drawable.ic_whatsapp),
-                                        contentDescription = null,
-                                    )
-                                }
-                            }
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        // image product
+                        val painterProductImage = rememberAsyncImagePainter(
+                            model = order?.product?.imageUrl
                         )
-                        Spacer(modifier = Modifier.height(24.dp))
+                        Image(
+                            painter = painterProductImage,
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(48.dp)
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        // product detail : name, price, bid
+                        Column(modifier = Modifier.fillMaxWidth()) {
+                            Spacer(modifier = Modifier.height(4.dp))
+                            val productName = order?.product?.name ?: ""
+                            Text(
+                                text = productName,
+                                style = MaterialTheme.typography.body1.copy(
+                                    fontWeight = FontWeight.Bold
+                                )
+                            )
+                            // product normal price
+                            Spacer(modifier = Modifier.height(4.dp))
+                            val productPrice = order?.product?.price ?: 0
+                            Text(
+                                text = "Rp $productPrice",
+                                style = MaterialTheme.typography.body1.copy(
+                                    fontWeight = FontWeight.Bold,
+                                    textDecoration = TextDecoration.LineThrough
+                                )
+                            )
+                            // buyer bid for selected product
+                            Spacer(modifier = Modifier.height(4.dp))
+                            val productBid = order?.bidPrice ?: 0
+                            Text(
+                                text = "Ditawar Rp $productBid",
+                                style = MaterialTheme.typography.body1.copy(
+                                    fontWeight = FontWeight.Bold
+                                )
+                            )
+                        }
                     }
                 }
             }
+            Spacer(modifier = Modifier.height(24.dp))
+            // button to contact buyer via whatsapp
+            PrimaryButton(
+                onClick = { /*TODO whatsapp*/ },
+                content = {
+                    Row {
+                        Text(
+                            text = stringResource(R.string.contact_whatsapp),
+                            style = MaterialTheme.typography.button.copy(
+                                textAlign = TextAlign.Center
+                            ),
+                            modifier = Modifier.fillMaxWidth(0.8f)
+                        )
+                        Icon(
+                            painter = rememberAsyncImagePainter(model = R.drawable.ic_whatsapp),
+                            contentDescription = null,
+                        )
+                    }
+                }
+            )
+            Spacer(modifier = Modifier.height(24.dp))
         }
     }
 }
