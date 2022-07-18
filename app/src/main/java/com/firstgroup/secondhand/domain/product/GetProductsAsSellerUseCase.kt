26d71eae09f44716f2141716a119d9153864e746
wiryadev <abrarwiryawan@gmail.com>
2022-07-18 18:52:10 +0700

    add UseCase related to wishlist

--- app/src/main/java/com/firstgroup/secondhand/domain/product/GetProductsAsSellerUseCase.kt
+++ app/src/main/java/com/firstgroup/secondhand/domain/product/GetProductsAsSellerUseCase.kt
@@ -1,7 +1,7 @@
 package com.firstgroup.secondhand.domain.product
 
 import com.firstgroup.secondhand.core.common.dispatchers.AppDispatcher
-import com.firstgroup.secondhand.core.common.dispatchers.SecondhandDispatchers
+import com.firstgroup.secondhand.core.common.dispatchers.SecondhandDispatchers.IO
 import com.firstgroup.secondhand.core.data.repositories.product.ProductRepository
 import com.firstgroup.secondhand.core.model.Product
 import com.firstgroup.secondhand.domain.UseCase
@@ -10,7 +10,7 @@ import javax.inject.Inject
 
 class GetProductsAsSellerUseCase @Inject constructor(
     private val productRepository: ProductRepository,
-    @AppDispatcher(SecondhandDispatchers.IO) private val ioDispatcher: CoroutineDispatcher,
+    @AppDispatcher(IO) private val ioDispatcher: CoroutineDispatcher,
 ) :  UseCase<Any, List<Product>>(ioDispatcher) {
 
     override suspend fun execute(param: Any): List<Product> {
