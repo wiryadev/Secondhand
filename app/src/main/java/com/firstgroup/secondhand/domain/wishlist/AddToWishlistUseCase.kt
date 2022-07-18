a27d4ac4ab6268717bddc94ff3b4ad520bf3c8b7
wiryadev <abrarwiryawan@gmail.com>
2022-07-18 18:52:10 +0700

    add UseCase related to wishlist

---
+++ app/src/main/java/com/firstgroup/secondhand/domain/wishlist/AddToWishlistUseCase.kt
@@ -0,0 +1,20 @@
+package com.firstgroup.secondhand.domain.wishlist
+
+import com.firstgroup.secondhand.core.common.dispatchers.AppDispatcher
+import com.firstgroup.secondhand.core.common.dispatchers.SecondhandDispatchers.IO
+import com.firstgroup.secondhand.core.data.repositories.wishlist.WishlistRepository
+import com.firstgroup.secondhand.core.model.BasicResponse
+import com.firstgroup.secondhand.domain.UseCase
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Inject
+
+class AddToWishlistUseCase @Inject constructor(
+    private val wishlistRepository: WishlistRepository,
+    @AppDispatcher(IO) private val ioDispatcher: CoroutineDispatcher,
+) : UseCase<Int, BasicResponse>(ioDispatcher) {
+
+    override suspend fun execute(param: Int): BasicResponse {
+        return wishlistRepository.addToWishlist(param)
+    }
+
+}
